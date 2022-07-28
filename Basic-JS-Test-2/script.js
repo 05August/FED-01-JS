10a5a5e6095727f55954bd5b022a92055d1bf859
05August <90166291+05August@users.noreply.github.com>
2022-07-28 23:22:29 +0700

    JS test 2

---
+++ Basic-JS-Test-2/script.js
@@ -0,0 +1,79 @@
+const $ = (selector) => document.getElementById(selector);
+let listNotify = new Array();
+let textHtmlNotify = '';
+
+
+
+
+const deleteItem = () => {
+  var close = document.getElementsByClassName("close");
+  var i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      listNotify = listNotify.filter(element => element.content !== listNotify[div.getAttribute('index')].content);
+      insertNotify();
+    }
+  }
+}
+
+
+
+
+const isFutureDay = (notifyDay) => {
+  const today = new Date();
+  return today.setHours(0, 0, 0, 0) <= notifyDay.setHours(0, 0, 0, 0) ? true : false;
+}
+
+const isToday = (notifyDay) => {
+  const today = new Date();
+  return today.setHours(0, 0, 0, 0) === notifyDay.setHours(0, 0, 0, 0) ? true : false;
+}
+
+const insertNotify = () => {
+  textHtmlNotify = '';
+  for (let i = 0; i < listNotify.length; i++) {
+    if (isToday(new Date(listNotify[i].date[0], listNotify[i].date[1] - 1, listNotify[i].date[2]))) {
+      textHtmlNotify += `<div class="item today" index="${i}">Ngày ${listNotify[i].date[2]}/${listNotify[i].date[1]}/${listNotify[i].date[0]} <p>${listNotify[i].content}</p><span class="close">x</span></span></div>`;
+    } else {
+      textHtmlNotify += `<div class="item" index="${i}">Ngày ${listNotify[i].date[2]}/${listNotify[i].date[1]}/${listNotify[i].date[0]} <p>${listNotify[i].content}</p><span class="close">x</span></span></div>`;
+    }
+  }
+  $('notify-list').innerHTML = textHtmlNotify;
+  localStorage.setItem('notify-list', JSON.stringify(listNotify));
+  deleteItem();
+}
+
+if(JSON.parse(localStorage.getItem('notify-list'))!==null){
+  listNotify=JSON.parse(localStorage.getItem('notify-list'));
+  insertNotify();
+}
+
+$('btnSave').onclick = function () {
+  const content = $('notify-content').value;
+  const day = $('notify-day').value;
+  const arrDay = day.split("-");
+  if (content === '') {
+    $('error-alert-content').style.display = 'block';
+  }
+  if (day === '') {
+    $('error-alert-day').style.display = 'block';
+  }
+  if (!isFutureDay(new Date(arrDay[0], arrDay[1] - 1, arrDay[2]))) {
+    $('error-alert-day').style.display = 'block';
+    $('error-alert-day').innerHTML = 'Bạn lựa chọn ngày nhắc chưa phù hợp';
+  }
+  else {
+    $('error-alert-content').style.display = 'none';
+    $('error-alert-day').style.display = 'none';
+    listNotify.push({ content: content, date: arrDay });
+    insertNotify();
+    var array = Object.keys(document.getElementsByTagName('input'))
+      .map(function (key) {
+        return document.getElementsByTagName('input')[key];
+      });
+    array.forEach(element => element.value = '');
+
+  }
+  deleteItem();
+}
