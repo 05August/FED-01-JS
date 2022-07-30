d27c4ad9559a91646b2d0d8c4c24a091f2324a00
05August <90166291+05August@users.noreply.github.com>
2022-07-31 00:12:37 +0700

    JS Basic Test 2

--- Basic-JS-Test-2/script.js
+++ Basic-JS-Test-2/script.js
@@ -1,23 +1,7 @@
 const $ = (selector) => document.getElementById(selector);
 let listNotify = new Array();
+listNotify=JSON.parse(localStorage.getItem('notify-list'));
 let textHtmlNotify = '';
-
-
-
-
-const deleteItem = () => {
-  var close = document.getElementsByClassName("close");
-  var i;
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      var div = this.parentElement;
-      listNotify = listNotify.filter(element => element.content !== listNotify[div.getAttribute('index')].content);
-      insertNotify();
-    }
-  }
-}
-
-
 
 
 const isFutureDay = (notifyDay) => {
@@ -30,11 +14,36 @@ const isToday = (notifyDay) => {
   return today.setHours(0, 0, 0, 0) === notifyDay.setHours(0, 0, 0, 0) ? true : false;
 }
 
+
+
+
+const deleteItem = () => {
+  var close = document.getElementsByClassName("close");
+  var i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      var indexOfListNotify =div.getAttribute('index');
+      if(isToday(new Date(listNotify[indexOfListNotify].date[0], listNotify[indexOfListNotify].date[1] - 1, listNotify[indexOfListNotify].date[2]))){
+        $('today-notify').innerHTML="";
+      }
+      listNotify = listNotify.filter(element => element.content !== listNotify[indexOfListNotify].content);
+      insertNotify();
+    }
+  }
+}
+
+
+
+
+
+
 const insertNotify = () => {
   textHtmlNotify = '';
   for (let i = 0; i < listNotify.length; i++) {
     if (isToday(new Date(listNotify[i].date[0], listNotify[i].date[1] - 1, listNotify[i].date[2]))) {
-      textHtmlNotify += `<div class="item today" index="${i}">Ngày ${listNotify[i].date[2]}/${listNotify[i].date[1]}/${listNotify[i].date[0]} <p>${listNotify[i].content}</p><span class="close">x</span></span></div>`;
+      textHtmlNotify += `<div class="item today" index="${i}">Ngày ${listNotify[i].date[2]}/${listNotify[i].date[1]}/${listNotify[i].date[0]} <p>${listNotify[i].content}</p><span class="close">x</span></div>`;
+      $('today-notify').innerHTML=`<div><p>${listNotify[i].content}</p></div>`;
     } else {
       textHtmlNotify += `<div class="item" index="${i}">Ngày ${listNotify[i].date[2]}/${listNotify[i].date[1]}/${listNotify[i].date[0]} <p>${listNotify[i].content}</p><span class="close">x</span></span></div>`;
     }
@@ -76,4 +85,10 @@ $('btnSave').onclick = function () {
 
   }
   deleteItem();
+}
+
+if(listNotify.length > 0){
+  listNotify = listNotify.filter(element => isFutureDay(new Date(element.date[0], element.date[1] - 1, element.date[2])));
+  insertNotify();
+
 }
